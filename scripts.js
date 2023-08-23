033bb16e2144412f59dcd3cd05f77a723abd8cd3
Graig Janssen <graig.janssen@gmail.com>
2023-08-23 13:49:17 -0400

    finalize mvp

--- scripts.js
+++ scripts.js
@@ -1,8 +1,21 @@
-
 // spin button
 const spinButton = document.querySelector('#spin-button');
 
-const baseReel = ['cat', 'ramen', 'sushi', 'shroom', 'cherry', 'bar', 'seven', 'ramen', 'cat', 'sushi', 'bar', 'cherry', 'seven', 'shroom', 'ramen', 'cat', 'sushi'];
+const baseReel = ['cat', 'ramen', 'sushi', 'shroom', 'cat', 'ramen', 'sushi', 'cherry', 'bar', 'ramen', 'seven', 'cat', 'bar', 'sushi', 'cherry', 'ramen', 'shroom', 'ramen', 'cat', 'sushi'];
+const winSong = new Audio('audio/win.mp3');
+const winOverlay = document.querySelector("#win-overlay");
+
+// To show the overlay when a win occurs
+function showWinOverlay() {
+  winOverlay.style.visibility = 'visible';
+  winOverlay.style.opacity = '1';
+}
+
+// To hide the overlay when needed
+function hideWinOverlay() {
+  winOverlay.style.visibility = 'hidden';
+  winOverlay.style.opacity = '0';
+}
 
 const slotMachine = {
   reel1: {
@@ -31,14 +44,23 @@ const slotMachine = {
       reel.element.innerHTML = `<img class="reel-item" src="img/${reel.symbols[reel.position]}.jpg" alt="${reel.symbols[reel.position]}"/>`;
     });
   },
+  spinSound: function() {
+    const variant = Math.floor((Math.random() * 6) + 1);
+    const sound = new Audio(`audio/spinSound${variant}.mp3`);
+    sound.play();
+  },
   spin: function() {
+    spinButton.classList.add('disabled');
+    this.spinSound();
     return new Promise(resolve => {
-      this.reel3.count = Math.floor((Math.random() * 5) + 36);
+      this.reel3.count = Math.floor((Math.random() * 5) + 27);
       this.reel2.count = this.reel3.count - (Math.floor(Math.random() * 5) + 3);
-      this.reel1.count = this.reel2.count - (Math.floor(Math.random() * 5) + 3);
-  
-      console.log('counts: ', this.reel3.count, this.reel2.count, this.reel1.count);
+      this.reel1.count = this.reel2.count - (Math.floor(Math.random() * 4) + 3);
+      let delay = 0
       for (let i = 0; i <= this.reel3.count; i++) {
+        if (i >= 20) {
+          delay++;
+        }
         setTimeout(() => {
           this.reels.forEach(reel => {
             if (i < reel.count) {
@@ -49,7 +71,7 @@ const slotMachine = {
           if (i === this.reel3.count) {
             resolve();
           }
-        }, i * 125);
+        }, i * (78 + delay));
       }
     })
   },
@@ -58,6 +80,24 @@ const slotMachine = {
     return this.reels.every(reel => {
       return reel.symbols[reel.position] === firstReelSymbol;
     });
+  },
+  checkOutcome: function() {
+    if (this.isWin()) {
+      winSong.play();
+      setTimeout(() => {
+        showWinOverlay();
+      }, 1650);
+      // Add event listener for the escape key
+      document.addEventListener('keydown', function(event) {
+        if (event.key === 'Escape') {  // Escape key is 27
+          hideWinOverlay();  // Assumes you have this function
+          if (!winSong.paused) {  // If the song is still playing
+            winSong.pause();  // Pause the song
+            winSong.currentTime = 0;  // Reset song to start
+          }
+        }
+      });
+    }
   },
   consoleDisplay: function() {
     const text = this.isWin() ? 'THE HEAT IS ON!!!' : 'fail';
@@ -69,12 +109,12 @@ const slotMachine = {
 }
 
 slotMachine.init();
-slotMachine.consoleDisplay();
 
 spinButton.addEventListener('click', () => {
   slotMachine.spin()
     .then(() => {
-      slotMachine.consoleDisplay();
+      spinButton.classList.remove('disabled');
+      slotMachine.checkOutcome();
     });
   
 });
